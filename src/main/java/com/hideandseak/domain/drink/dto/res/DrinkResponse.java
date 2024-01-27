5a0e3d81d6d847b649beda6998700dc36f24be96
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-01-27 21:09:40 +0900

    Fix :: Remove import

--- src/main/java/com/hideandseak/domain/drink/dto/res/DrinkResponse.java
+++ src/main/java/com/hideandseak/domain/drink/dto/res/DrinkResponse.java
@@ -2,11 +2,8 @@ package com.hideandseak.domain.drink.dto.res;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
-import java.util.List;
 
 @Getter
-@Setter
 @Builder
 public class DrinkResponse {
 
