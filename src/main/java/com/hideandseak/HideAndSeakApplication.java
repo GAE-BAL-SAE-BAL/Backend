584319fcca0e57aa5fae579106e242bfd5e12edb
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-01-27 14:57:35 +0900

    init

---
+++ src/main/java/com/hideandseak/HideAndSeakApplication.java
@@ -0,0 +1,13 @@
+package com.hideandseak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HideAndSeakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HideAndSeakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hideandseak/HideAndSeakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hideandseak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HideAndSeakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
